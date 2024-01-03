fe840b1cc79f51639a994d9105acffedbe0d0102
pedrodom300211 <pedrodinguez2003@gmail.com>
2024-01-03 14:03:09 -0300

    Segundo commit

--- CursoApis/Datos/VillaStore.cs
+++ CursoApis/Datos/VillaStore.cs
@@ -6,8 +6,8 @@ namespace CursoApis.Datos
     {
         public static List<VillaDto> VillaList = new List<VillaDto>
         {
-            new VillaDto{id=1, Nombre="Vista a la picina"},
-            new VillaDto{id=2, Nombre="Vista a la Playa"}
+            new VillaDto{id=1, Nombre="Vista a la picina",Ocupantes=3,MetrosCuadrados=50},
+            new VillaDto{id=2, Nombre="Vista a la Playa", Ocupantes=4, MetrosCuadrados=80}
 
         };
     }
