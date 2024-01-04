4a7795c3bd1ede745e3bc1261904688102cfdb5d
pedrodom300211 <pedrodinguez2003@gmail.com>
2024-01-04 11:51:01 -0300

    Added sql connection string, created tables in database and filled with some data

---
+++ CursoApis/Datos/ApliccationDboContext.cs
@@ -0,0 +1,49 @@
+﻿using CursoApis.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursoApis.Datos
+{
+    public class ApliccationDboContext:DbContext
+    {
+        public ApliccationDboContext(DbContextOptions<ApliccationDboContext> options):base(options)
+        {
+           
+        }
+        public DbSet<Villa> Villas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Villa>().HasData(
+               new Villa()
+               {
+                   Id = 1,
+                   Nombre = "Villa Real",
+                   Detalle = "Detalle de la Villa...",
+                   imageUrl = "",
+                   Ocupantes = 5,
+                   MetrosCuadrados = 50,
+                   Tarifa = 200,
+                   Amenidad = "",
+                   FechaActualizacion = DateTime.Now,
+                   FechaCreacion = DateTime.Now
+               },
+                new Villa()
+                {
+                    Id = 2,
+                    Nombre = "Villa Falsa",
+                    Detalle = "Detalle de la Villa...",
+                    imageUrl = "",
+                    Ocupantes = 5,
+                    MetrosCuadrados = 50,
+                    Tarifa = 200,
+                    Amenidad = "",
+                    FechaActualizacion = DateTime.Now,
+                    FechaCreacion = DateTime.Now
+                }
+               );
+
+               
+        }
+
+    }
+}
